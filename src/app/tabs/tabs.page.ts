3ba2f48993b5bf8e70059e0af71d88a746c59f89
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-09-07 15:04:00 -0300

    Ajuste nas tabs e adicionando o side menu

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+  tabCurrent: string ='';
 
   constructor() {}
+
+  onTabChanged(event:any) {
+    this.tabCurrent = event.tab;
+  }
 
 }
